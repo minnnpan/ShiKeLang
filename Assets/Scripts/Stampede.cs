5acc4ad76c4012c758451da2bdc84021f5d567f1
xSCx <31561391+xSCx@users.noreply.github.com>
2024-10-07 22:03:49 +0800

    踩踏

--- Assets/Scripts/Stampede.cs
+++ Assets/Scripts/Stampede.cs
@@ -1,7 +1,10 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using DG.Tweening;
+using Random = UnityEngine.Random;
+
 public interface StampedeState
 {
     void OnEnable();
@@ -16,6 +19,7 @@ public class Stampede : MonoBehaviour, StampedeState
     {
         if (!StampedeState)
         {
+            stampedeLocs = ReadChildTransforms(stampedeLoc);
             StampedeState = true;
             StartCoroutine(SpawnStampede());
             Debug.Log("开启踩踏");
@@ -27,23 +31,36 @@ public class Stampede : MonoBehaviour, StampedeState
         StopCoroutine(SpawnStampede());
         Debug.Log("关闭踩踏");
     }
+    //玩家
+    public GameObject player;
+    //距离玩家的Foot范围
+    public float playerFootMaxDistance;
+    //很多Foot
+    public GameObject[] stampedeFoots;
+    //很多Foot的影子
+    public GameObject[] stampedeShadow;
+    //很多生成点
+    public Transform stampedeLoc;
     //踩踏频率
     public float stampedeInterval = 3;
     //生成的数量上限
     public int stampedeMaxFoots = 3;
     //踩踏范围,长方形
-    public Vector2 spawnAreaSize = new Vector2(10f, 10f);
+    //public Vector2 spawnAreaSize = new Vector2(10f, 10f);
     //踩踏示意
-    public GameObject stampedeCircle;
+    //public GameObject stampedeCircle;
     //踩踏示意~落下时间
     public float stampedeTime = 1;
     //踩踏实体
-    public GameObject stampedeFoot;
+    //public GameObject stampedeFoot;
     //踩踏实体生成时高度
     public float stampedeHeight = 10;
     
-        private float elapsedTime = 0f;
+    private float elapsedTime = 0f;
     private float difficultyIncreaseInterval = 30f;
+
+    private Transform[] stampedeLocs;
+
     //每隔一段时间随机踩踏
     System.Collections.IEnumerator SpawnStampede()
     {
@@ -51,10 +68,12 @@ public class Stampede : MonoBehaviour, StampedeState
         {
             //生成随机数量
             int objectCount = Random.Range(1, stampedeMaxFoots + 1);
-
-            for (int i = 0; i < objectCount; i++)
+            //找到玩家周边范围内的多个位置
+            List<Vector3> selectedPos = GetRandomNearbyPositions(objectCount);
+            //生成Foot
+            for (int i = 0; i < selectedPos.Count; i++)
             {
-                SpawnFoot();
+                SpawnFoot(selectedPos[i]);
             }
 
             // 等待指定的时间间隔
@@ -69,37 +88,94 @@ public class Stampede : MonoBehaviour, StampedeState
         }
     }
 
-    private void SpawnFoot()
+    private void SpawnFoot(Vector3 spawnPosition)
     {
-        float random_x = Random.Range(-spawnAreaSize.x / 2, spawnAreaSize.x / 2);
-        float random_z = Random.Range(-spawnAreaSize.y / 2, spawnAreaSize.y / 2);
-        
-        Vector3 spawnPosition = new Vector3(random_x, 0, random_z);
-
-        //实例化 示意物体
-        GameObject go = Instantiate(stampedeCircle, spawnPosition, Quaternion.identity);
-        Destroy(go, stampedeTime);
+        //随机一只脚的影子 并实例化
+        int randomIndex_foot = Random.Range(0, stampedeShadow.Length);
+        spawnPosition.y += 0.2f;
+        GameObject go = Instantiate(stampedeShadow[randomIndex_foot], spawnPosition, Quaternion.Euler(-90, 0, 0));
+        go.transform.localScale = new Vector3(10f, 10f, 50f);
+        // 使用 DOTween 放大
+        go.transform.DOScale(new Vector3(90f, 90f, 50f), stampedeTime-0.06f);
+        Destroy(go, stampedeTime-0.05f);
 
         //实例化 下落物体
-        Vector3 spawnPosition2 = new Vector3(random_x, stampedeHeight, random_z);
-        GameObject go2 = Instantiate(stampedeFoot, spawnPosition2, Quaternion.identity);
-        go2.transform.DOMoveY(0, stampedeTime).SetEase(Ease.Linear)
-            .OnComplete(() => Destroy(go2));
+        Vector3 spawnPosition2 = new Vector3(spawnPosition.x, stampedeHeight, spawnPosition.z);
+        GameObject go2 = Instantiate(stampedeFoots[randomIndex_foot], spawnPosition2, Quaternion.identity);
+        AnimateFoot(go2);
     }
 
     private void IncreaseDifficulty()
     {
         stampedeInterval = Mathf.Max(stampedeInterval * 0.9f, 0.5f);
         stampedeMaxFoots = Mathf.Min(stampedeMaxFoots + 1, 10);
-        spawnAreaSize *= 1.1f;
+        //spawnAreaSize *= 1.1f;
     }
+    
+    //读取子物体的transform
+    Transform[] ReadChildTransforms(Transform ts)
+    {
+        int childCount = ts.childCount;
+        Transform[]childTransforms  = new Transform[childCount];
 
+        for (int i = 0; i < childCount; i++)
+        {
+            childTransforms[i] = ts.GetChild(i);
+        }
+
+        return childTransforms;
+    }
+    
+    //找玩家距离内的位置
+    List<Vector3> GetRandomNearbyPositions(int numberOfTransforms)
+    {
+        List<Vector3> nearbyPositions = new List<Vector3>();
+
+        foreach (var transform in stampedeLocs)
+        {
+            float distance = Vector3.Distance(player.transform.position, transform.position);
+            if (distance <= playerFootMaxDistance)
+            {
+                nearbyPositions.Add(transform.position);
+            }
+        }
+
+        List<Vector3> selectedPositions = new List<Vector3>();
+        int count = Mathf.Min(numberOfTransforms, nearbyPositions.Count);
+
+        for (int i = 0; i < count; i++)
+        {
+            int randomIndex = Random.Range(0, nearbyPositions.Count);
+            selectedPositions.Add(nearbyPositions[randomIndex]);
+            nearbyPositions.RemoveAt(randomIndex);
+        }
+
+        return selectedPositions;
+    }
+    
+    //Foot动画
+    void AnimateFoot(GameObject foot)
+    {
+        // 下落到指定位置
+        foot.transform.DOMoveY(0.8f, stampedeTime-0.12f).SetEase(Ease.InSine).OnComplete(() =>
+        {
+            // 左右摇晃
+            foot.transform.DOShakePosition(1f, new Vector3(0.1f, 0, 0.1f), 10, 90, false, true).OnComplete(() =>
+            {
+                // 抬起来
+                foot.transform.DOMoveY(stampedeHeight, stampedeTime).SetEase(Ease.OutFlash).OnComplete(() =>
+                {
+                    Destroy(foot);
+                });
+            });
+        });
+    }
     public void Reset()
     {
         OnDisable();
         stampedeInterval = 3;
         stampedeMaxFoots = 3;
-        spawnAreaSize = new Vector2(10f, 10f);
+        //spawnAreaSize = new Vector2(10f, 10f);
         elapsedTime = 0f;
     }
 }
